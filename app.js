e17c07fcad2ef9ea0a28d3dd9b5fb8586c8a9f89
arshisumair <arshisumair38@gmail.com>
2025-01-12 00:00:50 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,127 @@
+//Q # 1
+//var userName ="Mamduha"
+//alert(userName);
+//alert
+// Q # 2
+//alert("Welcome to my page");
+
+// Q # 3
+// var myName = "Arshi"
+// alert(myName);
+//Q#4
+//alert("Error! please enter a valid password");
+
+// Q # 5
+//var message ="Hello world"
+//alert(message);
+
+// Q # 6
+//var nameOfStudent ="Abdullah"
+// var ageOfStudent = "11 years old"
+// var course = "certified mobile application devoloper"
+// alert(nameOfStudent +  ageOfStuden + course);
+//Q#7
+//alert("Welcome to JS land... \n Happy Coading");
+
+//Q#8
+//alert("welcome to js land...");
+// alert("Happy coading! \n prevent this page from creating additinal dialogs");
+
+//Q#9
+// var pizza= "pizza"
+//alert("pizza \n pizz \n piz \n pi \n p");
+
+//Q#10
+// var email = "sohanaveed@gmail.com"
+// alert("my email address is "  + email );
+//alert("Hello ... I can run JS through my web browser's console");
+
+//Q#11
+//  var book = 'A  smarter \n  Way to Learn JavaScript';
+//  alert(" I am trying to learn from the book "+ book);
+
+//Q#12
+// alert("yah! Ican write HTML content through JavaScript");
+
+//Q#13
+// var desing = "“▬▬▬▬▬▬▬▬
+//Q # 1
+//var userName ="Mamduha"
+//alert(userName);
+//alert
+// Q # 2
+//alert("Welcome to my page");
+
+// Q # 3
+// var myName = "Arshi"
+// alert(myName);
+//Q#4
+//alert("Error! please enter a valid password");
+
+// Q # 5
+//var message ="Hello world"
+//alert(message);
+
+// Q # 6
+//var nameOfStudent ="Abdullah"
+// var ageOfStudent = "11 years old"
+// var course = "certified mobile application devoloper"
+// alert(nameOfStudent +  ageOfStuden + course);
+//Q#7
+//alert("Welcome to JS land... \n Happy Coading");
+
+//Q#8
+//alert("welcome to js land...");
+// alert("Happy coading! \n prevent this page from creating additinal dialogs");
+
+//Q#9
+// var pizza= "pizza"
+//alert("pizza \n pizz \n piz \n pi \n p");
+
+//Q#10
+// var email = "sohanaveed@gmail.com"
+// alert("my email address is "  + email );
+//alert("Hello ... I can run JS through my web browser's console");
+
+//Q#11
+//  var book = 'A  smarter \n  Way to Learn JavaScript';
+//  alert(" I am trying to learn from the book "+ book);
+
+//Q#12
+// alert("yah! Ican write HTML content through JavaScript");
+
+//Q#13
+// var desing = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+// alert(desing);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
